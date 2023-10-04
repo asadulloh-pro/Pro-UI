4d7e11be07c1fd64fef62769d873c88f3a359871
Asadullo <72871274+Asadullo2502@users.noreply.github.com>
2023-10-04 18:41:10 +0500

    add spiner component

--- src/components/ui/button/style.ts
+++ src/components/ui/button/style.ts
@@ -1,16 +1,19 @@
 import React from 'react';
-import { ButtonType } from '../../../types';
+import { ButtonProps } from '../../../types';
+
+const initialStyle =
+  'pro-ui-button border px-[15px] py-[4px] rounded-[6px] transition-all duration-300 ring-1 ring-opacity-10 flex items-center gap-[10px] disabled:pointer-events-none disabled:opacity-[0.5] active:scale-[0.9] shadow-md ';
 
 export function styleConfig(
-  type: ButtonType['typeEl']
+  type: ButtonProps['typeEl']
 ): React.ComponentProps<'button'>['className'] {
   switch (type) {
     case 'default':
-      return 'pro-ui-button border border-primary text-primary hover:border-main hover:text-main disabled:border-main disabled:text-main px-[15px] py-[4px] text-[14px] rounded-[6px] transition-all duration-300 shadow-md ring-1 ring-primary ring-opacity-10 active:scale-[0.9] flex items-center gap-[10px] disabled:pointer-events-none disabled:opacity-[0.5]';
+      return initialStyle + 'border-primary text-primary hover:border-main hover:text-main disabled:border-main disabled:text-main  text-[14px] ring-primary';
     case 'dashed':
-      return 'pro-ui-button border border-dashed border-primary text-primary hover:border-main hover:text-main disabled:border-main disabled:text-main px-[15px] py-[4px] text-[14px] rounded-[6px] transition-all duration-300 shadow-md ring-1 ring-primary ring-opacity-10 active:scale-[0.9] flex items-center gap-[10px] disabled:pointer-events-none disabled:opacity-[0.5]';
+      return initialStyle + 'border-dashed border-primary text-primary hover:border-main hover:text-main disabled:border-main disabled:text-main  text-[14px]   ring-primary';
     case 'primary':
-      return 'pro-ui-button border border-main bg-main text-background hover:opacity-[0.8]  px-[15px] py-[4px] text-[14px] rounded-[6px] transition-all duration-300 shadow-md ring-1 ring-main ring-opacity-10 active:scale-[0.9] flex items-center gap-[10px] disabled:pointer-events-none disabled:opacity-[0.5]';
+      return initialStyle + 'border-main bg-main text-background hover:opacity-[0.8] text-[14px]   ring-main';
     default:
       return;
   }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,13 @@
 import { HTMLProps, ReactNode } from "react"
 
-export type ButtonType = {
+export type ButtonProps = {
   typeEl?: "default" | "primary" | "dashed",
   type?: "button" | "submit" | "reset",
   loading?: boolean;
   prefix?: ReactNode;
 } & Omit<HTMLProps<HTMLButtonElement>, "type">
+
+export type SpinerProps = {
+  className?: string;
+  svgClassName?: string;
+}
